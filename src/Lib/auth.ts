83cb8fed3bf4320549958962887f0c9c179cced0
HaruCraft <luisaraniva2@gmail.com>
2024-11-05 14:13:54 -0600

    Clientes completo

--- src/Lib/auth.ts
+++ src/Lib/auth.ts
@@ -1,39 +1,39 @@
 async function verify(authcode: string | null): Promise<boolean> {
-    if (!authcode) {
-        return false;
-    }
+  if (!authcode) {
+    return false;
+  }
 
-    try {
-        const response = await fetch("http://localhost:8000/auth/verify", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${authcode}`,
-            },
-        });
+  try {
+    const response = await fetch("http://localhost:8000/auth/verify", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${authcode}`,
+      },
+    });
 
-        if (response.status === 200) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        console.error(error);
-        return false;
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
 async function userinfo(token) {
-    try {
-      const response = await fetch("http://localhost:8000/usuarios/me", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error:", error);
-    }
+  try {
+    const response = await fetch("http://localhost:8000/usuarios/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
   }
-export { verify , userinfo};
+}
+export { verify, userinfo };
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,10 @@
-export function onRequest (context, next) {
-    //is a cookie called token
-    context.locals.token = context.cookies.get('token')
-    next();
-};
+import { corsMiddleware } from "astrocors";
+
+export function onRequest(context, next) {
+  //is a cookie called token
+
+  context.locals.token = context.cookies.get("token");
+  next();
+}
+
+export default corsMiddleware;
